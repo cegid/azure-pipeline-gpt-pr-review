45fe853da26d260d394d688e91854e3efb0d5255
Guitton Nicolas <nguitton@cegid.com>
2024-06-11 15:51:51 +0200

    chore: Update .cspell.json and src/utils.ts

--- GPTPullRequestReview/src/utils.ts
+++ GPTPullRequestReview/src/utils.ts
@@ -1,9 +1,22 @@
 import * as tl from "azure-pipelines-task-lib/task";
 
+/**
+ * Retrieves the file extension from a given filename.
+ *
+ * @param {string} fileName - The name of the file.
+ * @returns {string} The file extension.
+ */
 export function getFileExtension(fileName: string) {
   return fileName.slice((fileName.lastIndexOf(".") - 1 >>> 0) + 2);
 }
 
+/**
+ * Retrieves the target branch name from the system variables.
+ * If the target branch name is not defined, it returns undefined.
+ * Otherwise, it removes the 'refs/heads/' prefix and prepends 'origin/' to the branch name.
+ *
+ * @returns {string | undefined} The target branch name or undefined if not set.
+ */
 export function getTargetBranchName() {
   let targetBranchName = tl.getVariable('System.PullRequest.TargetBranchName');
 
